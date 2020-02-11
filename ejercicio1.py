8e6e70a82cd415d8a38347fa7bfe17ff6f32c908
Andres <pinzon@outlook.es>
2020-02-10 21:18:12 -0500

    Mi primer ejercicio

---
+++ ejercicio1.py
@@ -0,0 +1,5 @@
+edad= int(input("Ingrese su edad\n"))
+cantAños=int(input("Ingrese la cantidad de años que quiere que pasen\n"))
+edadFutura=edad+cantAños
+print("Edad actual: "+str(edad))
+print("Edad futura: "+str(edadFutura))
